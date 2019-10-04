605cc35f3885d681747bbc2d14f22f16a6b03041
Brandon Kim <bkim2192@stu.d214.org>
2019-10-04 13:32:00 -0500

    Initial Commit

---
+++ CollegeBuilderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollegeBuilderApp
+//
+//  Created by Brandon Kim on 10/4/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
